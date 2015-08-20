d1ed87ca4e13582153576988701102189a1dafd3
Tamas Molnar <stiron@gmail.com>
2015-08-20 12:14:59 +0200

    Initial commit

---
+++ cookbooks/vagrant/metadata.rb
@@ -0,0 +1,7 @@
+name             'vagrant'
+maintainer       'Tamas Molnar'
+maintainer_email 'stiron@gmail.com'
+license          'apache2'
+description      'Installs/Configures vagrant'
+long_description 'Installs/Configures vagrant'
+version          '0.0.1'
